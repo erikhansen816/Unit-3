3e29fa2baf1fbaf631fa276893417aaee85e4b28
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-27 13:48:39 -0400

    Updated from Brython Server: 9/27/2017 1:48:38 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,9 @@
+#Erik Hansen
+#9/27/2017
+#favorites.py - looping favorite words
 
+favword = input('Enter your favorite word: ')
+favnum = int(input('Enter your favorite number: '))
+
+for i in range(1,favnum+1):
+    print(favword)
