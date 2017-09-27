88138734a40cb63f330f0fcf2c897e96979e2255
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-27 14:00:20 -0400

    Updated from Brython Server: 9/27/2017 2:00:20 PM

--- countdown.py
+++ countdown.py
@@ -2,8 +2,6 @@
 #9/27/2017
 #countdown.py- counting down and saying boom
 
-for i in range(5,-1,-1):
-    if i > 0:
-        print(i)
-    else:
-        print('BOOM!')
+for i in range(5,0,-1):
+    print(i)
+print('BOOM!')
