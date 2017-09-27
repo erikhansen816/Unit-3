3702ba834f6f3b49fcd5efedd458da510ad8c712
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-27 13:28:44 -0400

    Updated from Brython Server: 9/27/2017 1:28:44 PM

--- loopDemo.py
+++ loopDemo.py
@@ -1,1 +1,12 @@
+#Erik Hansen
+#9/27/2017
+#loopDemo.py - learning loops
 
+"""
+#print I love computer science 5 times
+for i in range(1,6):
+    print('I <3 computer science')"""
+    
+#print the numbers from 1 to 20
+for i in range(1,21):
+    print(i)
