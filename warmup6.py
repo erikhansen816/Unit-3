8c07ae72cecfc4acaa0c070a5765767c34f0e5a7
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-28 14:19:57 -0400

    Updated from Brython Server: 9/28/2017 2:19:55 PM

--- warmup6.py
+++ warmup6.py
@@ -1,1 +1,13 @@
+#Erik Hansen
+#9/28/2017
+#warmup6.py - warming up the computer stuff knowledge
 
+for i in range(1,11,1):
+    print(i, 'Mississippi')
+    
+i = 1
+while i <= 10:
+    print(i, 'Mississippi')
+    i = i+1
+    
+    
