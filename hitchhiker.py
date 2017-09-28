7312ba37f6736097bc60dc17be449e7bcd31c400
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-28 14:36:24 -0400

    Updated from Brython Server: 9/28/2017 2:36:22 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,9 @@
+#Erik Hansen
+#9/28/2017
+#hitchhiker.py - answer to everything is 42
 
+while True:
+    text = input("Ask a question or type 'quit': ")
+    print(42)
+    if text == 'quit':
+        break
