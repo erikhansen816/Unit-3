81429c98a71fd1110f6de9c7c96333cf9c26c0c0
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-29 14:32:35 -0400

    Updated from Brython Server: 9/29/2017 2:32:32 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,14 @@
+#Erik Hansen
+#9/29/2017
+#divisors.py
+
+num = int(input('Enter a number: '))
+total = 0
+for i in range(1,num):
+    if num%i==0:
+        total = total + i
+if total == num:
+    print('Perfect')
+else:
+    print('Not perfect')
 
