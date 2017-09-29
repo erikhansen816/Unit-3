e995a87688d45c012a4fb1f343cd5fb860cc7f71
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-29 14:36:47 -0400

    Updated from Brython Server: 9/29/2017 2:36:44 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -7,13 +7,11 @@ tries = 0
 num = randint(1,100)
 while True:
     guess = int(input('Guess a number between 1 and 100: '))
+    tries = tries + 1
     if guess>num:
         print('Your guess is too high')
     elif guess<num:
         print('Your guess is too low')
     else:
-        tries = tries + 1
-    
         break
-    tries = tries + 1
 print('You got it in', tries, 'tries')
