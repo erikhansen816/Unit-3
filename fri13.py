66005c4ba18e957ddb0098b1271eaf7e631a1293
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-02 14:53:50 -0400

    Updated from Brython Server: 10/2/2017 2:53:49 PM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,24 @@
+#Erik Hansen
+#10/2/2017
+#fri13.py
+from calendar import weekday
+from datetime import date
 
+day = date.today().day
+month = date.today().month
+year = date.today().year
+print(month, day, year)
+friday13=0
+
+if day>13 and month!=12:
+    month = month + 1
+
+while friday13<=10:
+    if weekday(year,month,13) == 4:
+        friday13 += 1
+        print(month,'13,', year)
+    if month!=12:
+        month += 1
+    else:
+        month = 1
+        year +=1
