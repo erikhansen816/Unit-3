32d5d5a84bc56fcd215998debeee08356bb71ec1
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-04 13:49:36 -0400

    Updated from Brython Server: 10/4/2017 1:49:33 PM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,10 @@
+#Erik Hansen
+#10/2/2017
+#StringLoopDemo.py
 
+word = input('Enter a word: ')
+for ch in word:
+    if ch == 'a' or ch == 'A' or ch == 'e' or ch == 'E' or ch == 'i' or ch == 'I' or ch == 'o' or ch == 'O' or ch == 'u' or ch == 'U':
+        print(ch.upper())
+    else:
+        print(ch)
