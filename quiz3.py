b4d1f4e48ea967e5ad4f114fff2d10adac661e65
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-13 14:24:06 -0400

    Updated from Brython Server: 10/13/2017 2:24:04 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,15 @@
-
+for i in range(-5,6):
+    print(i)
+n=18
+while n<=32:
+    print(n)
+    n=n+2
+total = 0
+for j in range(13,332,2):
+    total = total+j
+print(total)
+while True:
+    word = input('Enter a word: ')
+    if 'z' in word:
+        break
+    
